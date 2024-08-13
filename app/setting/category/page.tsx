4b547b792b1e27084660c740d5b54ce5eb6d3dfa
nparadise <s_nakwon@naver.com>
2024-08-13 17:30:25 +0900

    refactor: search parameters

--- app/setting/category/page.tsx
+++ app/setting/category/page.tsx
@@ -1,7 +1,11 @@
 import AddMainCategoryForm from "@/app/components/setting/category/AddMainCategoryForm";
 import AddSubCategoryForm from "@/app/components/setting/category/AddSubCateogoryForm";
+import CategoryList from "@/app/components/setting/category/CategoryList";
+import DeletedCategoryList from "@/app/components/setting/category/DeletedCategoryList";
+import DeleteForm from "@/app/components/setting/category/DeleteForm";
 import EditMainCategoryForm from "@/app/components/setting/category/EditMainCategoryForm";
 import EditSubCategoryForm from "@/app/components/setting/category/EditSubCategoryForm";
+import RecoverForm from "@/app/components/setting/category/RecoverForm";
 import Title from "@/app/components/Title";
 import { fetchCategories } from "@/app/lib/database";
 import { Metadata } from "next";
@@ -9,13 +13,14 @@ import Link from "next/link";
 
 interface Props {
   searchParams: {
-    addMain: string;
-    addSub: string;
-    editMain: string;
-    editSub: string;
-    delete: string;
+    target: "main" | "sub";
+    add: "true" | "false";
+    edit: "true" | "false";
+    delete: "true" | "false";
+    recover: "true" | "false";
+    includeSub: "true" | "false";
     mainId: string;
-    subId: string;
+    id: string;
     name: string;
   };
 }
@@ -42,149 +47,65 @@ export default async function Page({ searchParams }: Props) {
       {/* Buttons */}
       <div>
         <Link
-          href={{ pathname: "/setting/category", query: { addMain: "true" } }}
+          href={{
+            pathname: "/setting/category",
+            query: { add: "true", target: "main" },
+          }}
           className="main-button inline-block"
           replace
         >
           주 카테고리 추가
         </Link>
         <Link
-          href={{ pathname: "/setting/category", query: { addSub: "true" } }}
+          href={{
+            pathname: "/setting/category",
+            query: { add: "true", target: "sub" },
+          }}
           className="main-button ml-1 inline-block"
           replace
         >
           하위 카테고리 추가
         </Link>
       </div>
-      <section className="mt-1 grid grid-cols-4 items-stretch border-2 border-black">
-        {/* head row */}
-        <div className="border-b-2 border-black px-2 text-lg font-bold">
-          Main Category
-        </div>
-        <div className="border-b-2 border-black px-2 text-lg font-bold">
-          Sub Category
-        </div>
-        <div className="border-b-2 border-black px-2 text-lg font-bold">
-          Edit
-        </div>
-        <div className="border-b-2 border-black px-2 text-lg font-bold">
-          Delete
-        </div>
-        {/* category lists */}
-        {categoryList.map((main) => {
-          return (
-            <>
-              <MainRow name={main.name} id={main.id} key={main.name} />
-              {main.children.map((sub) => {
-                return (
-                  <SubRow
-                    name={sub.name}
-                    id={sub.id}
-                    parentId={main.id}
-                    key={sub.name}
-                  />
-                );
-              })}
-            </>
-          );
-        })}
-      </section>
-      {searchParams.addMain === "true" && <AddMainCategoryForm />}
-      {searchParams.addSub === "true" && (
+      <CategoryList categoryList={categoryList} />
+      <DeletedCategoryList categoryList={categoryList} />
+
+      {/* Modals */}
+      {searchParams.add === "true" && searchParams.target === "main" && (
+        <AddMainCategoryForm />
+      )}
+      {searchParams.add === "true" && searchParams.target === "sub" && (
         <AddSubCategoryForm mainCategoryList={mainCategoryList} />
       )}
-      {searchParams.editMain === "true" && (
+      {searchParams.edit === "true" && searchParams.target === "main" && (
         <EditMainCategoryForm
           id={searchParams.mainId}
           name={searchParams.name}
         />
       )}
-      {searchParams.editSub === "true" && (
+      {searchParams.edit === "true" && searchParams.target === "sub" && (
         <EditSubCategoryForm
           mainCategoryList={mainCategoryList}
           subCategory={{
-            id: searchParams.subId,
+            id: searchParams.id,
             name: searchParams.name,
             parentId: searchParams.mainId,
           }}
         />
       )}
-    </>
-  );
-}
-
-function MainRow({ name, id }: { name: string; id: number }) {
-  return (
-    <>
-      <div className="border-r border-t border-black px-2">{name}</div>
-      <div className="border-r border-t border-black px-2"></div>
-      <div className="border-t border-black px-2">
-        <EditButton category="main" data={{ name: name, id: id }} />
-      </div>
-      <div className="border-t border-black px-2">
-        <DeleteButton />
-      </div>
-    </>
-  );
-}
-
-function SubRow({
-  name,
-  id,
-  parentId,
-}: {
-  name: string;
-  id: number;
-  parentId: number;
-}) {
-  return (
-    <>
-      <div className="border-r border-black px-2"></div>
-      <div className="border-r border-black px-2">{name}</div>
-      <div className="px-2">
-        <EditButton
-          category="sub"
-          data={{ name: name, id: id, parentId: parentId }}
+      {searchParams.delete === "true" && (
+        <DeleteForm
+          target={searchParams.target}
+          data={{ id: searchParams.id, name: searchParams.name }}
         />
-      </div>
-      <div className="px-2">
-        <DeleteButton />
-      </div>
+      )}
+      {searchParams.recover === "true" && (
+        <RecoverForm
+          target={searchParams.target}
+          data={{ id: searchParams.id, name: searchParams.name }}
+          includeSub={searchParams.includeSub ?? "false"}
+        />
+      )}
     </>
-  );
-}
-
-function EditButton({
-  category,
-  data,
-}: {
-  category: "main" | "sub";
-  data: { name: string; id: number; parentId?: number };
-}) {
-  const query =
-    category === "main"
-      ? { editMain: "true", mainId: data.id.toString(), name: data.name }
-      : {
-          editSub: "true",
-          mainId: data.parentId?.toString(),
-          subId: data.id.toString(),
-          name: data.name,
-        };
-
-  return (
-    <Link
-      href={{ pathname: "/setting/category", query: query }}
-      className="main-button block"
-    >
-      수정
-    </Link>
-  );
-}
-
-function DeleteButton() {
-  return (
-    <Link href="" className="alert-button block">
-      삭제
-    </Link>
   );
 }
